a459449375e8d71a98ca0899159e62ab4d6775f0
KuroiX <34186407+KuroiX@users.noreply.github.com>
2017-12-08 23:02:55 +0100

    player script

---
+++ GravityRunner/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+	public float runSpeed;
+	public float changeSpeed;
+	public float jumpPower;
+	Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (((Vector2.right * runSpeed) + (Vector2.right * runSpeed)*Input.GetAxis("Horizontal") * changeSpeed)*Time.deltaTime);
+
+		if (Input.GetAxis ("Jump") != 0) {
+			rigidBody.AddForce (Vector2.up * jumpPower);
+		}
+	}
+}
