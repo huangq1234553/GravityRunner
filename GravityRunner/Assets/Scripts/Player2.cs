63326ce855fe19c9a2e639d38bbb087af18b1815
GuyKost <33894595+GuyKost@users.noreply.github.com>
2017-12-08 23:40:42 +0100

    Player2 Code
    implemented gravity shifter with saftey. Hope it works.

---
+++ GravityRunner/Assets/Scripts/Player2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour {
+
+    bool safety = true;
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(5);
+        safety = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (safety)
+            {
+                Physics.gravity *= -1;
+                safety = false;
+            } else
+            {
+                StartCoroutine(Cooldown());
+            }
+        }
+    }
+}
